ddcab46275ff389907746282d303f1571cef1226
Angel Edgardo Gamez Sanchez <18170323@itculiacan.edu.mx>
2022-11-10 09:30:10 -0700

    Initial Commit

---
+++ ProyectoAEGS/ProyectoAEGSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoAEGSApp.swift
+//  ProyectoAEGS
+//
+//  Created by CCDM30 on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoAEGSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
